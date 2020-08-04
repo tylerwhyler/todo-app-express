af2c4406b58822606f7cab7743dbbaf309b0a1ed
tylerwhyler <tyjiman@gmail.com>
2020-08-04 08:58:08 -0700

    finished express api routes

--- express-API/routes/todoRoutes.js
+++ express-API/routes/todoRoutes.js
@@ -6,11 +6,11 @@ const { restart } = require("nodemon")
 
 // Get
 router.get("/todos", (req, res) => {
-    Todos.find((err, document) => {
+    Todos.find((err, doc) => {
         if (err) {
             res.status(404).json({ message: "get todos error:", errors: `${err}` })
         } else {
-            res.status(200).json(document)
+            res.status(200).json(doc)
         }
     })
 })
@@ -18,11 +18,11 @@ router.get("/todos", (req, res) => {
 // Show route (gets one item)
 
 router.get("/todo/:id", (req, res) => {
-    Todos.findById(req.params.id, (err, document) => {
+    Todos.findById(req.params.id, (err, doc) => {
         if (err) {
             res.status(404).json({ message: "error getting todo:", errors: `${err}` })
         } else {
-            res.status(200).json(todo)
+            res.status(200).json(doc)
         }
     })
 })
@@ -33,8 +33,8 @@ router.post("/todo", (req, res) => {
 
     todo
         .save()
-        .then(document => {
-            res.status(200).json(document)
+        .then(doc => {
+            res.status(200).json(doc)
         })
         .catch(err => {
             res.status(400).json({ message: "unable to post", errors: `${err}`})
@@ -42,9 +42,38 @@ router.post("/todo", (req, res) => {
 })
 
 // Patch
+router.patch("/todo/:id", (req, res) => {
+    const { done } = req.body
+
+    Todos.findById(req.params.id, (err, doc) => {
+        if (err) {
+            res.status(404).json({ message: "Todo not found. Could not update", errors: `${err}` })
+        } else {
+            doc.done = done
+
+            doc
+                .save()
+                .then(doc => {
+                    res.status(200).json({ message: `Todo Item: "${doc.title}" updated`})
+                })
+                .catch(err => {
+                    res.status(400).json({ message: "unable to update", err: `${err}`})
+                })
+        }
+    })
+})
 
 
 // Delete
+router.delete("/todo/:id", (req, res) => {
+    Todos.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (err) {
+            res.status(404).json({ message: "Could not delete, todo not found", errors: `${err}`})
+        } else {
+            res.status(200).json({ message: "removed, goodbye"})
+        }
+    })
+})
 
 
 module.exports = router
